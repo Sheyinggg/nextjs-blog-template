508ecc5b6ff3b094132b685d1e0225883ff8cf3f
Sheyinggg <sheying4126@gmail.com>
2025-06-19 12:31:11 +0800

    feat: 测试修改内容

--- src/app/blog/[...slug]/page.tsx
+++ src/app/blog/[...slug]/page.tsx
@@ -1,41 +1,37 @@
-import { allBlogs } from "content-collections"
-import type { Metadata } from "next"
-import { absoluteUrl, formatDate } from "@/lib/utils"
-import { notFound } from "next/navigation"
-import { getTableOfContents } from "@/lib/toc"
-import { DashboardTableOfContents } from "@/components/toc"
+import { allBlogs } from 'content-collections'
+import type { Metadata } from 'next'
+import { absoluteUrl, formatDate } from '@/lib/utils'
+import { notFound } from 'next/navigation'
+import { getTableOfContents } from '@/lib/toc'
+import { DashboardTableOfContents } from '@/components/toc'
 import { MDXRemote } from 'next-mdx-remote-client/rsc'
 import count from 'word-count'
-import { components } from "@/components/mdx-components"
+import { components } from '@/components/mdx-components'
 import remarkGfm from 'remark-gfm'
 import remarkMath from 'remark-math'
 import rehypeKatex from 'rehype-katex'
-import rehypeHighlight from 'rehype-highlight';
-import rehypeSlug from 'rehype-slug';
+import rehypeHighlight from 'rehype-highlight'
+import rehypeSlug from 'rehype-slug'
 import 'highlight.js/styles/github-dark.min.css'
-import GiscusComments from "@/components/giscus-comments"
-import { GoToTop } from "@/components/go-to-top"
-import 'katex/dist/katex.min.css';
-import { config } from "@/lib/config";
+import GiscusComments from '@/components/giscus-comments'
+import { GoToTop } from '@/components/go-to-top'
+// import 'katex/dist/katex.min.css';
+import { config } from '@/lib/config'
 
 type BlogsPageProps = {
-  params: Promise<{slug: string[]}>
+  params: Promise<{ slug: string[] }>
   searchParams: Promise<{ [key: string]: string | string[] | undefined }>
 }
 
 const options = {
   mdxOptions: {
-      remarkPlugins: [remarkGfm, remarkMath],
-      rehypePlugins: [
-        rehypeKatex,
-        rehypeHighlight,
-        rehypeSlug
-      ],
+    remarkPlugins: [remarkGfm, remarkMath],
+    rehypePlugins: [rehypeKatex, rehypeHighlight, rehypeSlug]
   }
 }
 
 async function getBlogsFromParams(slugs: string[]) {
-  const slug = slugs?.join("/") || ""
+  const slug = slugs?.join('/') || ''
   const blog = allBlogs.find((blog: any) => blog.slug === slug)
 
   if (!blog) {
@@ -45,7 +41,9 @@ async function getBlogsFromParams(slugs: string[]) {
   return blog
 }
 
-export async function generateMetadata({ params }: BlogsPageProps): Promise<Metadata> {
+export async function generateMetadata({
+  params
+}: BlogsPageProps): Promise<Metadata> {
   const { slug } = await params
   const blog = await getBlogsFromParams(slug)
 
@@ -61,12 +59,12 @@ export async function generateMetadata({ params }: BlogsPageProps): Promise<Meta
       title: blog.title,
       description: blog.title,
       type: config.seo.openGraph.type,
-      url: absoluteUrl("/" + blog.slug),
+      url: absoluteUrl('/' + blog.slug),
       images: [
         {
           url: config.site.image
-        },
-      ],
+        }
+      ]
     },
     twitter: {
       card: config.seo.twitter.card,
@@ -75,22 +73,22 @@ export async function generateMetadata({ params }: BlogsPageProps): Promise<Meta
       images: [
         {
           url: config.site.image
-        },
+        }
       ],
-      creator: config.seo.twitter.creator,
-    },
+      creator: config.seo.twitter.creator
+    }
   }
 }
 
 export async function generateStaticParams(): Promise<string[]> {
   // @ts-ignore
   return allBlogs.map((blog: any) => ({
-    slug: blog.slug.split('/'),
+    slug: blog.slug.split('/')
   }))
 }
 
 export default async function BlogPage(props: BlogsPageProps) {
-  const { slug } = await props.params;
+  const { slug } = await props.params
   const blog = await getBlogsFromParams(slug)
 
   if (!blog) {
@@ -100,26 +98,30 @@ export default async function BlogPage(props: BlogsPageProps) {
   const toc = await getTableOfContents(blog.content)
 
   return (
-    <main className="relative py-6 max-w-full md:max-w-6xl mx-auto lg:gap-10 lg:py-8 xl:grid xl:grid-cols-[1fr_300px]">
-      <div className="max-w-4xl mx-auto w-full px-6">
-        <div className="my-8">
-          <h1 className="text-[32px] font-bold">{blog.title}</h1>
+    <main className='relative py-6 max-w-full md:max-w-6xl mx-auto lg:gap-10 lg:py-8 xl:grid xl:grid-cols-[1fr_300px]'>
+      <div className='max-w-4xl mx-auto w-full px-6'>
+        <div className='my-8'>
+          <h1 className='text-[32px] font-bold'>{blog.title}</h1>
         </div>
 
-        <div className="my-4">
-          <p className="text-sm">
+        <div className='my-4'>
+          <p className='text-sm'>
             {formatDate(blog.date)} · {count(blog.content)} 字
           </p>
         </div>
 
-        <div className="">
-          <MDXRemote source={blog.content} components={components} options={options} />
+        <div className=''>
+          <MDXRemote
+            source={blog.content}
+            components={components}
+            options={options}
+          />
         </div>
 
         <GiscusComments />
       </div>
-      <div className="hidden text-sm xl:block">
-        <div className="sticky top-16 -mt-6 h-[calc(100vh-3.5rem)]">
+      <div className='hidden text-sm xl:block'>
+        <div className='sticky top-16 -mt-6 h-[calc(100vh-3.5rem)]'>
           <div className="h-full overflow-auto pb-10 flex flex-col justify-between mt-16 [&::-webkit-scrollbar]:hidden [-ms-overflow-style:'none'] [scrollbar-width:'none']">
             <DashboardTableOfContents toc={toc} />
             <GoToTop />
@@ -127,5 +129,5 @@ export default async function BlogPage(props: BlogsPageProps) {
         </div>
       </div>
     </main>
-  );
+  )
 }
